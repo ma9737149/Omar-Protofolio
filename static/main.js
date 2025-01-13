597227358ec0857517edd02aef2cd4ccb44e23a6
Ahmed <132069136+ma9737149@users.noreply.github.com>
2025-01-13 22:27:52 +0200

    Update main.js
    Fixing Background Bug

--- static/main.js
+++ static/main.js
@@ -1,5 +1,6 @@
 icon = document.querySelector('.logo .icon')
 container = document.querySelector('.container')
+body = document.querySelector('body')
 
 function setLocalStorage(mode){
     localStorage.setItem('mode' , mode)
@@ -12,10 +13,12 @@ icon.addEventListener("click", (e) => {
     if (e.currentTarget.classList.contains("fa-moon")) {
         e.currentTarget.classList.replace("fa-moon", "fa-sun");
         container.classList.add("dark");
+        body.classList.add("dark");
         setLocalStorage('dark')
     } else {
         e.currentTarget.classList.replace("fa-sun", "fa-moon");
         container.classList.remove("dark");
+        body.classList.remove('dark')
         setLocalStorage('light')
     }
 });
@@ -24,11 +27,13 @@ document.addEventListener('DOMContentLoaded' , () => {
     if (localStorage.getItem('mode') === 'light') {
         icon.classList.replace("fa-sun", "fa-moon");
         container.classList.remove("dark");
+        body.classList.remove('dark')
     }
 
     else if (localStorage.getItem('mode') === 'dark') {
         icon.classList.replace("fa-moon", "fa-sun");
         container.classList.add("dark");
+        body.classList.add('dark')
     }
 
     else
